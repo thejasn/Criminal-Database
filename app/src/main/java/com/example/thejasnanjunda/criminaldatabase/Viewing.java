a36d8b02f4897c29b146e58b02214ad281654cf4
Thejas Nanjunda <thejasn.416@gmail.com>
2017-10-22 22:17:24 +0530

    wrote the fragment initialization for viewing the criminal

--- app/src/main/java/com/example/thejasnanjunda/criminaldatabase/Viewing.java
+++ app/src/main/java/com/example/thejasnanjunda/criminaldatabase/Viewing.java
@@ -2,29 +2,71 @@ package com.example.thejasnanjunda.criminaldatabase;
 
 import android.app.Activity;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Viewing extends AppCompatActivity{
 
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewing);
+        toolbar = (Toolbar) findViewById(R.id.toolbarView);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        tabLayout = (TabLayout) findViewById(R.id.tabsView);
+        viewPager = (ViewPager) findViewById(R.id.viewpagerView);
+        setupViewPager(viewPager);
 
-        DBHelper dbHelper = new DBHelper(this);
-        List<Criminal> opList = dbHelper.getAllPrisoners();
-        String log="";
-        for(Criminal c: opList) {
-            log = "Name: " + c.getF_name();
-            Log.d("list" + Integer.toString(c.getUID()), log);
-        }
+        tabLayout.setupWithViewPager(viewPager);
+
+
 
     }
+    private void setupViewPager(ViewPager viewPager) {
+       Viewing.ViewPagerAdapter adapter = new Viewing.ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new  view_Criminal(), "Criminal");
+        viewPager.setAdapter(adapter);
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
